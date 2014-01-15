d09ecebba2dc4244421fb41da7727855594f05f4
Gerald Bullard Jr <gerald@thereactorworks.com>
2014-01-15 04:22:35 -0500

    more updates

--- responsive-calendar-events/www/admin/inc/rci/class/event_class_add.php
+++ responsive-calendar-events/www/admin/inc/rci/class/event_class_add.php
@@ -1,23 +1,53 @@
 <?php
-  $exists = (mysql_num_rows(mysql_query("SHOW COLUMNS FROM " . DB_PREFIX . "groups LIKE 'events'"))) ? true : false;
-  if ($exists !== true) {
+  $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  
+  $eventsTableExists = (mysql_num_rows(mysql_query("SHOW COLUMNS FROM " . DB_PREFIX . "groups LIKE 'events'"))) ? true : false;
+  if ($eventsTableExists !== true) {
     mysql_query("ALTER TABLE " . DB_PREFIX . "_groups ADD events TINYINT(1) UNSIGNED NOT NULL DEFAULT '1'");
   }
   
   require_once('inc/class/Event.class.php');
   
-  // Get the access level for events
-  $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  $sql = "SELECT events FROM " . DB_PREFIX . "groups WHERE id = :id";
+  if (isset($_POST['events'])) {
+    $_SESSION['postevents'] = ($_POST['events'] != '') ? $_POST['events'] : 0;
+  }
   
-  $st = $conn->prepare ( $sql );
-  $st->bindValue( ":id", User::getGroupID($_SESSION['authuser']), PDO::PARAM_INT );
-  $st->execute();
+  if (isset($_GET['newGroup']) && $_GET['newGroup'] != '') {
+    // Update the events access level for new groups
+    $update = "UPDATE " . DB_PREFIX . "groups SET events = :events WHERE id = :id";  
+    $st = $conn->prepare ( $update );
+    $st->bindValue( ":events", $_SESSION['postevents'], PDO::PARAM_INT );
+    $st->bindValue( ":id", $_GET['newGroup'], PDO::PARAM_INT );
+    $st->execute();
+  } else if (isset($_POST['events']) && !isset($_GET['newGroup'])) {
+    // Update the events access level for existing groups
+    $update = "UPDATE " . DB_PREFIX . "groups SET events = :events WHERE id = :id";  
+    $st = $conn->prepare ( $update );
+    $st->bindValue( ":events", ($_POST['events'] != '') ? $_POST['events'] : 0, PDO::PARAM_INT );
+    $st->bindValue( ":id", $_GET['editId'], PDO::PARAM_INT );
+    $st->execute();
+  }
   
-  $row = $st->fetch();
+  // Get the current edit access level for events
+  $editaccess = "SELECT events FROM " . DB_PREFIX . "groups WHERE id = :id";  
+  $ea = $conn->prepare ( $editaccess );
+  $ea->bindValue( ":id", $_GET['groupId'], PDO::PARAM_INT );
+  $ea->execute();
+  
+  $ea = $ea->fetch();
     
-  $_SESSION['access']->events = $row['events'];
+  $_SESSION['editaccess'] = $ea['events'];
+  
+  // Get the logged in admin access level for events
+  $sessionaccess = "SELECT events FROM " . DB_PREFIX . "groups WHERE id = :id";  
+  $sa = $conn->prepare ( $sessionaccess );
+  $sa->bindValue( ":id", User::getGroupID($_SESSION['authuser']), PDO::PARAM_INT );
+  $sa->execute();
+  
+  $sa = $sa->fetch();
+    
+  $_SESSION['access']->events = $sa['events'];
   
   $conn = null;
 ?>
