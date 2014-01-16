5a5074edca8d655c84be1a2bf6ffd5c62586fd9f
Gerald Bullard Jr <gerald@thereactorworks.com>
2014-01-15 22:48:41 -0500

    more search updates

--- site-search/inc/rci/templatebottom/search_templatebottom_add.php
+++ site-search/inc/rci/templatebottom/search_templatebottom_add.php
@@ -16,7 +16,6 @@
     var params = [];
     if (location.search) {
       var parts = location.search.substring(1).split('&');
-
       for (var i = 0; i < parts.length; i++) {
         var nv = parts[i].split('=');
         if (!nv[0]) continue;
@@ -27,6 +26,9 @@
     var links = [];
     for (var key in params) {
       if (key == 'noresults') {
+        var noresults = true;
+      } else if (key == 'minterms') {
+        var minterms = true;
       } else if (key == 'results') {
         var results = urldecode(params[key]);
       } else {
@@ -41,16 +43,20 @@
         }
       }
       cnt++;
-    }
-    
+    }    
     if (links != '') {
       $("#search_form").hide();
       $("#search_terms").html('<h4>Search Results for "' + results + '"</h4>');
       $("#search_results").html('<ul class="search-result-ul">' + links.join("") + '</ul>');
     } else {
-      $("#search_results").html('<h4 class="search-h4">No Search Results</h4>');
-    }
-  
+      if (noresults) {
+        $("#search_results").html('<h4 class="search-h4">No Search Results</h4>');
+      } else if (minterms) {
+        $("#search_results").html('<h4 class="search-h4">Search Term Too Short. Try Again.</h4>');
+      } else {
+        header("Location: 404.html");
+      }
+    }  
     // Getting URL var by its nam
     // var byName = $.getUrlVar('name');
   });
